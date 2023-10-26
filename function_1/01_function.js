044a0d5910a74aeac1bafbe00979d2c5a6748a65
pradeep-singh315 <pradeepthakurkanha@gmail.com>
2023-10-26 21:09:03 +0530

    Make another function which returns a value.

--- function_1/01_function.js
+++ function_1/01_function.js
@@ -3,4 +3,11 @@ function addTwoNumbers(a, b){
         console.log("Sum is : ", a+b);
 }
 
-addTwoNumbers(3,7)
+addTwoNumbers(3,7) 
+
+function square(a)
+{
+    return a*a
+}
+let numSquare = square(4)
+console.log(numSquare);
