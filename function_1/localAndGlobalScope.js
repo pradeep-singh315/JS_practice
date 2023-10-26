d76379450f001eb46edbd98b487540d3cf287cda
pradeep-singh315 <pradeepthakurkanha@gmail.com>
2023-10-26 21:53:05 +0530

    Read about local and global scope..

---
+++ function_1/localAndGlobalScope.js
@@ -0,0 +1,14 @@
+let a = 30
+var b = 45
+ const c= 56
+ if(true){
+    let a = 29
+    var b = 78
+    //c = 94
+    console.log(a);
+    console.log(b);
+    console.log(c);
+ }
+ console.log(a);
+ console.log(b);
+ console.log(c);
