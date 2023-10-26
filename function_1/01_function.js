78c56dbea62f8b63b9d1a4194ca9ca7c218b91b5
pradeep-singh315 <pradeepthakurkanha@gmail.com>
2023-10-26 21:39:37 +0530

    Use array in functions..

--- function_1/01_function.js
+++ function_1/01_function.js
@@ -5,9 +5,21 @@ function addTwoNumbers(a, b){
 
 addTwoNumbers(3,7) 
 
+//Function which returns something..
 function square(a)
 {
     return a*a
 }
 let numSquare = square(4)
 console.log(numSquare);
+
+// Functions with Array
+let newArray = [1,2,3,4,5,6]
+function addArray(arr)
+{
+    let sum = arr[0] + arr[1] + arr[2] + arr[3] + arr[4] + arr[5]
+    console.log(sum);
+}
+addArray(newArray)
+// Here, addArray ----> reference
+// and, addArray() ------> execution
