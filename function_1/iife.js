7a28e5ea9d5bddc464e63a35815cc9fd809cafe8
pradeep-singh315 <pradeepthakurkanha@gmail.com>
2023-10-28 20:36:58 +0530

    Build another iife which takes parameter..

--- function_1/iife.js
+++ function_1/iife.js
@@ -1,3 +1,9 @@
 (() => {
     console.log(`I have completed 24 tutorials of Chai Or Js..`)
-})()
+})();
+// IIFE --> Immediately Invoke Function Expression
+// It can call where he declared...
+
+(cityName = (name) => {
+    console.log(`You basically live in ${name}...`);
+})("Aligarh")
