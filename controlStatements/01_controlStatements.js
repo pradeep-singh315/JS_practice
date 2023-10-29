349ba398eb9f6d90d35b911aa1ee739f49146516
pradeep-singh315 <pradeepthakurkanha@gmail.com>
2023-10-29 21:05:28 +0530

    Use if-else in simple condition..

---
+++ controlStatements/01_controlStatements.js
@@ -0,0 +1,8 @@
+let isGraduate = true
+if(isGraduate)
+{
+    console.log(`You can take admission in post graduation...`);
+}
+else{
+    console.log(`No, You are not elegible to take admission in post graduation...`);
+}
