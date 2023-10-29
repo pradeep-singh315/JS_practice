5028bcafab1fe08e2ac3e108afae40479acdc1fe
pradeep-singh315 <pradeepthakurkanha@gmail.com>
2023-10-29 21:09:47 +0530

    use if-else if in another example...

--- controlStatements/01_controlStatements.js
+++ controlStatements/01_controlStatements.js
@@ -6,3 +6,18 @@ if(isGraduate)
 else{
     console.log(`No, You are not elegible to take admission in post graduation...`);
 }
+
+// Another example----
+let age = 56
+if(age <= 20)
+{
+    console.log(`Your age is less than 20..`);
+}
+else if(age > 20 && age <= 40)
+{
+    console.log(`Your age is between 20 and 40..`);
+}
+else
+{
+    console.log(`Your age is more than 40.....`);
+}
