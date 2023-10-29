9217a875010ce0de14bee95b7e722bd0a8f8226a
pradeep-singh315 <pradeepthakurkanha@gmail.com>
2023-10-29 21:18:15 +0530

    example of nullish coalescing operator..

--- controlStatements/02_controlStatements.js
+++ controlStatements/02_controlStatements.js
@@ -2,4 +2,10 @@
 let a = 35
 let b = 56
 
-b > a ? console.log(`b is greater than a..`):console.log(`a is greater than b`);;
+b > a ? console.log(`b is greater than a..`):console.log(`a is greater than b`);
+
+// Nullish Coalescing Operator -------------
+let college = null
+let defaultCollege = "D S College"
+let result = college ?? defaultCollege
+console.log(result);
