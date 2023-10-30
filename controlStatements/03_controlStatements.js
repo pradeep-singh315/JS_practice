ba085be4d58e56e5f1f2e17cf6dac0ac727b0152
pradeep-singh315 <pradeepthakurkanha@gmail.com>
2023-10-30 20:22:01 +0530

    Simple example of switch case...

---
+++ controlStatements/03_controlStatements.js
@@ -0,0 +1,27 @@
+let day = 5
+switch (day) {
+    case 1:
+        console.log(`It is Sunday..`);
+        break;
+    case 2:
+        console.log(`It is Monday..`);
+        break;
+    case 3:
+        console.log(`It is Tuesday...`);
+        break;
+    case 4:
+        console.log(`It is Wednesday..`);
+        break;
+    case 5:
+        console.log(`It is Thursday...`);
+        break;
+    case 6:
+        console.log(`It is Friday...`);
+        break;
+    case 7:
+        console.log(`It is Saturday...`);
+        break;
+    default:
+        console.log(`You entered a wrong number...`);
+        break;
+}
