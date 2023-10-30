000d6e745b59c2cd5fc78d32fb50eab99bbd134b
pradeep-singh315 <pradeepthakurkanha@gmail.com>
2023-10-30 23:56:01 +0530

    Use for-in loop in my code..

--- loops/forIn.js
+++ loops/forIn.js
@@ -1,1 +1,21 @@
 // for in ------------
+// It is basically used in objects and it also used in arrays..
+
+let student = {
+    name : "Pradeep Singh",
+    rollNo : 83,
+    indian : true
+}
+
+for (const key in student) {
+    console.log(key);
+    console.log(student[key]);
+        
+    }
+
+
+let arr = [2, 78, 98, 5, 32, 90]
+for(const everyNumber in arr)
+{
+    console.log(`${arr[everyNumber]}`);
+}
