2f30bf3ee8c11f8460b2e1aa0c1a20cfd0f47259
pradeep-singh315 <pradeepthakurkanha@gmail.com>
2023-11-04 19:44:25 +0530

    Read about DOM..

--- documentObjectModel/more_about_DOM/dom_01.js
+++ documentObjectModel/more_about_DOM/dom_01.js
@@ -1,1 +1,6 @@
-const parentElement = document.querySelector("parent")
+const parentElement = document.querySelector(".parent")
+console.log(parentElement.getAttribute('class'));//We can get value of any attribute..
+parentElement.setAttribute("id", "parent-id")//We can set any attribute with the help of this function..
+console.log(parentElement.getAttribute('id'));
+
+//InnerText And TextContent
