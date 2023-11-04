79897b34ddf1d1b75937b639cd283133cbffcd12
pradeep-singh315 <pradeepthakurkanha@gmail.com>
2023-11-04 20:08:58 +0530

    Dom manupilation...

--- documentObjectModel/more_about_DOM/dom_01.js
+++ documentObjectModel/more_about_DOM/dom_01.js
@@ -4,3 +4,24 @@ parentElement.setAttribute("id", "parent-id")//We can set any attribute with the
 console.log(parentElement.getAttribute('id'));
 
 //InnerText And TextContent
+console.log(parentElement.textContent);// it can be return the content whose display is none..
+console.log(parentElement.innerText);//return an actual content..
+
+//Chids of an element
+const firstElement = parentElement.children[0]
+console.log(parentElement.children); // returns all the elements of parentElement
+console.log(firstElement.nextElementSibling);
+
+// Nodes 
+console.log(`Nodes --->  ${parentElement.childNodes}`);
+
+// Access by class names...
+const childDiv = document.getElementsByClassName("child-div")
+console.log(childDiv);
+const arrChild = Array.from(childDiv)
+const colorArray = ["red", "aqua", "yellow", "sandybrown", "indigo"]
+let i = 0
+arrChild.forEach((element) => {
+    element.style.backgroundColor = colorArray[i]
+    i++;
+})
